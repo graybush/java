5c58559ee14f22f48ef9f6624e8fca7f8f64e1ea
graybush <bpbeatty@gmail.com>
2018-08-12 20:33:15 -0500

    Initial Project Euler setup with maven build tool

---
+++ euler/src/main/java/com/euler/Euler.java
@@ -0,0 +1,13 @@
+package com.euler;
+
+/**
+ * Hello world!
+ *
+ */
+public class Euler 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
