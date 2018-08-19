47eb38e93616e9b267ff2ce08420abd2531f9dc1
centos <bpbeatty@gmail.com>
2018-08-18 21:01:41 -0500

    Addresses build warnings
    Moves problem 1 menu from main to P001.

--- euler/src/main/java/com/euler/Euler.java
+++ euler/src/main/java/com/euler/Euler.java
@@ -2,19 +2,18 @@ package com.euler;
 
 import java.util.Scanner;
 
-// import euler.P001;
 
 /**
- * Hello world!
+ * Project Euler!
  *
  */
 public class Euler {
 
   // create a single shared scanner for keyboard input
-  private Scanner scanner = new Scanner( System.in );
+  private Scanner scanner = new Scanner(System.in);
 
-  public static void main( String[] args ){
-
+  public static void main(String[] args){
+    System.out.println("\nWelcome to Project Euler!\n");
     Euler euler = new Euler();
     while (true) {
       euler.display();
@@ -23,48 +22,37 @@ public class Euler {
   }
 
   private void display() {
-    System.out.println( "Welcome to Project Euler! \n" 
-        + "Please select your desired application:  \n"
+    System.out.println("Please select your desired application:  \n"
         + "1: Multiples of 3 and 5\n"
-        + "4: Exit\n");
+        + "00: Exit\n");
 
     try {
       int choice = scanner.nextInt();
       scanner.nextLine();
       switch (choice) {
         case 1:
-          try {
-            P001 p001 = new P001();
-            p001.display();
-            int limit = scanner.nextInt();
-            int ans = p001.multiples3and5(limit);
-            System.out.println(ans + "\n");
-          } catch (Exception e) {
-            System.out.println(e);
-          }
-
+          P001 p001 = new P001();
+          p001.display();
           break;
         case 2:
           //
           break;
-        case 4:
+        case 0:
           this.exit();
           break;
         default:
-          System.out.println( "Invalid selection." );
+          System.out.println("\nInvalid input detected.\n");
           //
       }
     } catch (Exception e) {
       scanner.nextLine();
-      System.out.println("Invalid input detected.\n");
+      System.out.println("\nInvalid input detected.\n");
     }
-
   }
-
 
   private void exit() {
     System.out.println( "Exiting..." );
-    System.exit(1);
+    System.exit(0);
   }
 
 }
--- euler/src/main/java/com/euler/P001.java
+++ euler/src/main/java/com/euler/P001.java
@@ -1,15 +1,36 @@
 package com.euler;
 
+import java.util.Scanner;
+
 public class P001 {
+
+  private Scanner scanner = new Scanner(System.in);
 
   public void display() {
     System.out.println(
-        "This function will find the sum of all the natrual \n"
-        + "numbers below a given limit.\n"
-        + "What is the limit? \n");
+        "\nThis function will find the sum of all the natural \n"
+        + "numbers below a given limit.\n");
+    getInput();
   }
 
-  public static int multiples3and5(int limit) {
+  private void getInput() {
+    boolean state = true;
+    while (state) {
+      try {
+        System.out.println("What is the limit? \n");
+        int limit = scanner.nextInt();
+        int ans = multiples3and5(limit);
+        System.out.println("Answer: " + ans + "\n");
+        state = false;
+      } catch (Exception e) {
+        scanner.nextLine();
+        System.out.println("\nInvalid input detected.\n");
+      }
+
+    }
+  }
+
+  private int multiples3and5(int limit) {
     int total = 0;
     for ( int i = 0; i < limit; i++ ) {
       if ( i % 3 == 0 || i % 5 == 0 ) {
